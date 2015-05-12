a0b6e3c73521cfa07bfaf6e5eedcb2ee4be390bc
misaelmallada <misaelmallada@gmail.com>
2015-05-12 17:02:39 -0300

    cambios

---
+++ BusinessLogicLayer/TenantControllers/BLUsuario.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogicLayer.TenantInterfaces;
+using DAL.DAL_Tenant;
+using DAL.IDAL_Tenant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crosscutting.EntityTenant;
+
+namespace BusinessLogicLayer.TenantControllers
+{
+    public class BLUsuario : IBLUsuario
+    {
+        private IDALUsuario _dal = new DALUsuario();
+
+        public Boolean ExisteUsuario(String tenant,String email) {
+            return _dal.ExisteUsuario(tenant , email);
+        }
+
+        public void RegistrarUsuario(String tenant, Usuario usr)
+        {
+             _dal.RegistrarUsuario(tenant, usr);
+        }
+
+        public Usuario LoginUsuario(String valor_tenant, String email,String password){
+
+            return _dal.LoginUsuario(valor_tenant, email, password);
+        }
+    }
+}
