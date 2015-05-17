df95cc1f23629b51b46ccd684346db80b3c05a7c
misaelmallada <misaelmallada@gmail.com>
2015-05-17 15:32:38 -0300

    nada

--- Site/Global.asax.cs
+++ Site/Global.asax.cs
@@ -14,43 +14,16 @@ namespace Site
     {
         protected void Application_Start()
         {
-            ingresarHosts();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-           
 
-           // JobScheduler.Start();
+
+            // JobScheduler.Start();
         }
 
-        private void ingresarHosts()
-        {
-            try 
-	        {	
-                AgregarHost("www");
-                AgregarHost("");
-                AgregarHost("sitio");
-	            }
-	            catch (Exception)
-	            {
-		
-		            throw;
-	            }
-            
-          
-            
-            throw new NotImplementedException();
-        }
 
-        private void AgregarHost(string dominio)
-        {
-            var systemPath = Environment.GetFolderPath(Environment.SpecialFolder.System);
-            var path = Path.Combine(systemPath, @"drivers\etc\hosts");
-            using (var stream = new StreamWriter(path, true, Encoding.Default))
-            {
-                stream.WriteLine(@"127.0.0.1    " + dominio + ".chebay.com");
-            }
-        }
     }
 }
